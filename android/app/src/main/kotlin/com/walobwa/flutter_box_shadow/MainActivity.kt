94746bbd6ca2cfc1006ab3ae8de311e7d0809ee7
Ivy-Walobwa <ivywalobwa@gmail.com>
2022-03-18 07:42:31 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/walobwa/flutter_box_shadow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.walobwa.flutter_box_shadow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade300,
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 150,
+            child: const Icon(Icons.downhill_skiing),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
